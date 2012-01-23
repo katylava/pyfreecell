20a958ffe653d8e7cf8b605e29a47a8f8e39eb5f
katy lavallee <katylava@gmail.com>
2012-01-23 02:01:42 -0600

    started programming freecell game

---
+++ freecell.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python
+
+from carddeck import Card, Deck, CardSuit, CardRank
+
+class FreecellCard(Card):
+
+    def __init__(self, rank, suit):
+        super(FreecellCard).__init__(self, rank, suit)
+
+    def __repr__(self):
+        return self.color(8)
+
+
+class FreecellDeck(Deck):
+
+    def __init__(self):
+        super(FreecellDeck).__init__(self)
+        basic_cards = self.cards
+        self.cards = []
+        for c in basic_cards:
+            self.cards.append(FreecellCard(c.rank, c.suit))
+
+
+class NoFreecellsError(Exception):
+    pass
+
+class FreeCellOccupiedError(Exception):
+    pass
+
+class InvalidColumnCardError(Exception):
+    pass
+
+class InvalidFoundationCardError(Exception):
+    pass
+
+
+class CardStack:
+
+    def __init__(self, cards):
+        self.cards = cards
+
+    def top_card(self):
+        try:
+            return self.cards[-1]
+        except IndexError:
+            return None
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def remove_top_card(self):
+        return self.cards.pop()
+
+    def remove_card(self):
+        return self.remove_top_card()
+
+    @property
+    def length(self):
+        return len(self.cards)
+
+
+class FoundationPile(CardStack):
+
+    def __init__(self, suit):
+        self.cards = []
+        self.suit = CardSuit(suit)
+
+    def rank(self):
+        top = self.top_card()
+        if top:
+            return top.rank
+        else:
+            return None
+
+    def add_card(self, card):
+        if card == self.next_card():
+            self.cards.append(card)
+        else:
+            raise InvalidFoundationCardError
+
+    def next_card(self):
+        top = self.top_card()
+        if not top:
+            return FreecellCard(rank=CardRank('Ace'), suit=self.suit)
+        else:
+            return FreecellCard(rank=CardRank(top.rank.next_rank()),
+                                suit=self.suit)
+
+
+class AltDescCardColumn(CardStack):
+
+    def add_card(self, card):
+        if card in self.allowed_cards():
+            self.cards.append(card)
+        else:
+            raise InvalidColumnCardError
+
+    def allowed_cards(self):
+        top = self.top_card()
+        rank = top.rank.prev_rank()
+        if top.color == 'red':
+            return [
+                FreecellCard(rank, CardSuit('Spades')),
+                FreecellCard(rank, CardSuit('Clubs')),
+            ]
+        else:
+            return [
+                FreecellCard(rank, CardSuit('Hearts')),
+                FreecellCard(rank, CardSuit('Diamonds')),
+            ]
+
+
+class Freecells:
+    cells = [None, None, None, None]
+
+    def __init__(self):
+        pass
+
+    def free(self):
+        return self.cells.count(None)
+
+    def add_card(self, card, position=None):
+        if self.free():
+            if not position:
+                position = self.cells.index(None)
+            if self.cells[position] == None:
+                self.cells[position] = card
+                return position
+            else:
+                raise FreeCellOccupiedError
+        else:
+            raise NoFreecellsError
+
+    def get_card_at_position(self, position):
+        return self.cells[position]
+
+    def remove_card_from_position(self, position):
+        card = self.cells[position]
+        self.cells[position] = None
+        return card
+
+    def remove_card(self, card):
+        position = self.cells.index(card)
+        self.cells[position] = None
+
+
+class FreecellGame():
+    columns = []
+    freecells = Freecells()
+    foundation = {
+        'S': FoundationPile('Spades'),
+        'H': FoundationPile('Hearts'),
+        'D': FoundationPile('Diamonds'),
+        'C': FoundationPile('Clubs'),
+    }
+    history = []
+
+    def __init__(self):
+        deck = FreecellDeck()
+        deck.shuffle()
+        columns = deck.deal(8)
+        for c in columns:
+            self.columns.append(AltDescCardColumn(c))
+
+    def move(self, moves):
+        moves = list(moves)
+        cols = list('asdfjkl;')
+        cells = list('qwert')
+        found = 'u'
+        while len(moves) >= 2:
+            fr, to = (moves.pop(0), moves.pop(0))
+            if fr in cols and to in cols:
+                self.move_from_column_to_column(cols.index(fr), cols.index(to))
+            elif fr in cols and to in cells:
+                fcpos = None if to == 't' else cells.index(to)
+                self.move_to_freecell(cols.index(fr), fcpos)
+            elif fr in cols and to in found:
+                self.move_from_column_to_foundation(cols.index(fr))
+            elif fr in cells and to in cols:
+                self.move_from_freecell_to_column(cells.index(fr), cols.index(to))
+            elif fr in cells and to in found:
+                self.move_from_freecell_to_foundation(cells.index(fr))
+
+
+    def freecell_count(self):
+        return self.freecells.free()
+
+    def move_to_freecell(self, from_col, position=None):
+        card = self.columns[from_col].top_card()
+        try:
+            self.freecells.add_card(card, position)
+        except NoFreecellsError:
+            print 'No free cells'
+        except FreeCellOccupiedError:
+            print 'Already a card in that cell'
+        else:
+            self.columns[from_col].remove_top_card()
+
+    def move_from_freecell_to_column(self, fc, col):
+        card = self.freecells.get_card_at_position(fc)
+        try:
+            self.columns[col].add_card(card)
+        except InvalidColumnCardError:
+            print 'Bad move'
+        else:
+            self.freecells.remove_card(card)
+
+    def move_from_column_to_column(self, from_col, to_col):
+        card = self.columns[from_col].top_card()
+        try:
+            self.columns[to_col].add_card(card)
+        except InvalidColumnCardError:
+            print 'Bad move'
+        else:
+            self.columns[from_col].remove_top_card()
+
+    def move_from_freecell_to_foundation(self, fc):
+        card = self.freecells.get_card_at_position(fc)
+        try:
+            self.foundation[card.suit.c].add_card(card)
+        except InvalidFoundationCardError:
+            print 'Invalid foundation card'
+        else:
+            self.freecells.remove_card(card)
+
+    def move_from_column_to_foundation(self, col):
+        card = self.columns[col].top_card()
+        try:
+            self.foundation[card.suit.c].add_card(card)
+        except InvalidFoundationCardError:
+            print 'Invalid foundation card'
+        else:
+            self.columns[col].remove_top_card()
+
+    def draw_board(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    pass
